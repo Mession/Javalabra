080e11a9df67fe66a6d1548f87cc77b2846e68bb
Juho Leinonen <leju@melkki.cs.helsinki.fi>
2013-01-15 19:42:08 +0200

    Ekan deadlinen tehtavat

---
+++ NBJavalabraprojekti/src/nb/javalabra/projekti/NBJavalabraProjekti.java
@@ -0,0 +1,13 @@
+
+package nb.javalabra.projekti;
+
+/**
+ *
+ * @author Mession
+ */
+public class NBJavalabraProjekti {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
