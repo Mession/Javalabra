f7702442bb9bd56accbdac87269f9b9a086ac785
Juho Leinonen <leju@melkki.cs.helsinki.fi>
2013-01-15 20:08:29 +0200

    projekti

---
+++ NBJavalabra/src/nbjavalabra/NBJavalabra.java
@@ -0,0 +1,13 @@
+
+package nbjavalabra;
+
+/**
+ *
+ * @author Mession
+ */
+public class NBJavalabra {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
